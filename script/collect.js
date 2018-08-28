a810cf488d4855b1c6aec32ff36360a3ac645b5b
TiagoDanin <TiagoDanin@outlook.com>
2018-08-28 13:49:28 -0300

    Improved filter

--- script/collect.js
+++ script/collect.js
@@ -5,65 +5,62 @@ const path = require('path')
 const download = require('download')
 const walk = require('walk-sync').entries
 
-const platform = ["macOS", "Windows", "Linux"]
-
-const editor = ["Xcode", "Vim", "VisualStudioCode", "Emacs", "NotepadPP", "SublimeText", "Eclipse", "JetBrains"]
-
-const language = ["Actionscript", "Android", "Go", "Swift", "Jekyll", "Ruby", "Unity", "Kotlin", "Java", "Node", "Python", "C", "C++"]
+const filter = {
+	platform: ["macOS", "Windows", "Linux"],
+	editor: ["Xcode", "Vim", "VisualStudioCode", "Emacs", "NotepadPP", "SublimeText", "Eclipse", "JetBrains"],
+	language: ["Actionscript", "Android", "Go", "Swift", "Jekyll", "Ruby", "Unity", "Kotlin", "Java", "Node", "Python", "C", "C++"]
+}
 
 async function collect() {
-    const tempDir = path.join(__dirname, `./temp/`)
-    const docsDir = path.join(__dirname, `../docs/.vuepress/public/data.json`)
-    const docsMdDir = path.join(__dirname, `../docs/gitignore.md`)
-    const gitignoreUrl = `https://github.com/github/gitignore/archive/master.zip`
-    let data = {
-        platform: [],
-        editor: [],
-        language: []
-    }
-    await download(gitignoreUrl, tempDir, {
-        extract: true
-    })
-    walk(tempDir, {
-            directories: false
-        })
-        .filter(file => path.extname(file.relativePath.toLowerCase()) == '.gitignore')
-        .forEach(file => {
-            let name = path.basename(file.relativePath, '.gitignore')
-            let content = fs.readFileSync(file.fullPath, "utf-8")
-            let file_content = "##  " + name + "\n" + "```" + "\n\n" + content + "\n\n" + "```" + "\n\n"
-            fs.appendFile(docsMdDir, file_content, function (err) {
-                if (err) {
-                    console.log(err)
-                }
-            });
-            if (platform.includes(name)) {
-                data.platform.push({
-                    name: name,
-                    path: file.relativePath,
-                    content: content
-                })
-            }
-            if (editor.includes(name)) {
-                data.editor.push({
-                    name: name,
-                    path: file.relativePath,
-                    content: content
-                })
-            }
-            if (language.includes(name)) {
-                data.language.push({
-                    name: name,
-                    path: file.relativePath,
-                    content: content
-                })
-            }
-        })
-    fs.writeFileSync(
-        docsDir,
-        JSON.stringify(data, null, 2)
-    )
-    fs.remove(tempDir)
+	const tempDir = path.join(__dirname, `./temp/`)
+	const docsDir = path.join(__dirname, `../docs/.vuepress/public/data.json`)
+	const docsMdDir = path.join(__dirname, `../docs/gitignore.md`)
+	const gitignoreUrl = `https://github.com/github/gitignore/archive/master.zip`
+	let data = {
+		platform: [],
+		editor: [],
+		language: []
+	}
+	await download(gitignoreUrl, tempDir, {
+		extract: true
+	})
+	walk(tempDir, {
+			directories: false
+		})
+		.filter(file => path.extname(file.relativePath.toLowerCase()) == '.gitignore')
+		.forEach(file => {
+			let name = path.basename(file.relativePath, '.gitignore')
+			let content = fs.readFileSync(file.fullPath, "utf-8")
+			let file_content = "##  " + name + "\n" + "```" + "\n\n" + content + "\n\n" + "```" + "\n\n"
+			fs.appendFile(docsMdDir, file_content, function(err) {
+				if (err) {
+					console.log(err)
+				}
+			})
+			for (type in filter) {
+				if (filter[type].includes(name)) {
+					data[type].push({
+						name: name,
+						path: file.relativePath,
+						content: content
+					})
+				}
+			}
+		})
+
+		let allList = ['platform', 'editor']
+		allList.forEach(type => {
+			data[type].push({
+				name: 'All',
+				path: false,
+				content: data[type].reduce((_, next) => _.content ? _.content + next.content : _ + next.content)
+			})
+		})
+	fs.writeFileSync(
+		docsDir,
+		JSON.stringify(data, null, 2)
+	)
+	fs.remove(tempDir)
 }
 
 collect()
